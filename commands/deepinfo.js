a571e826972c97411fe8fb8359161f8f80004d69
MaterBolado <miguelmanuelgarts@gmail.com>
2026-09-13 20:14:54 +0100

    Add files via upload

--- commands/deepinfo.js
+++ commands/deepinfo.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const fetch = require("node-fetch");
+const fetch = global.fetch;
+
 
 module.exports = {
     data: new SlashCommandBuilder()
